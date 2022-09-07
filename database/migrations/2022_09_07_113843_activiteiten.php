8f6ed3c46c94392e242d3d7238343bfaa9234e50
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-09-07 14:54:08 +0200

    database

---
+++ database/migrations/2022_09_07_113843_activiteiten.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activiteiten', function (Blueprint $table) {
+            $table->id('ID');
+            $table->string('activiteitnaam');
+            $table->string('activeitomschrijving');
+            $table->boolean('activeit-eten');
+            $table->integer('activiteitmindeelnemers');
+            $table->integer('activiteitmaxdeelnemers');
+            $table->float('activiteitkosten');
+            $table->string('activiteitbenodigheden');
+            $table->string('activiteitbegintijd');
+            $table->string('activiteiteindtijd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activiteiten');
+    }
+};
