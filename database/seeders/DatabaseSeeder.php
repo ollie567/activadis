58dc7c48b024b2bcd1368fab8369716ceea84087
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-09-08 11:29:45 +0200

    data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,12 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('gebruikers')->insert([
-//            'gebruikersnaam' => 45,
-//            'ticket_name' => 'volwassen',
-//            'ticket_category' => 'ticket_2',
-//            'ticket_text1' => 'boven 12 jaar',
-//            'ticket_text2' => 'toegang tot alle attracties'
-//        ]);
+        DB::table('gebruikers')->insert([
+            'gebruikeremail' => 'admin@gmail.com',
+            'gebruikernaam' => 'admin',
+            'gebruikerwachtwoord' => 'adminww',
+            'is_admin' => true,
+        ]);
+        DB::table('gebruikers')->insert([
+            'gebruikeremail' => 'user@gmail.com',
+            'gebruikernaam' => 'user',
+            'gebruikerwachtwoord' => 'userww',
+            'is_admin' => false,
+        ]);
+        DB::table('activiteiten')->insert([
+            'activiteitnaam' => 'jetskiëen',
+            'activiteitlocatie' => 'stroombroek in braamt',
+            'activeitomschrijving' => 'In braamt ga je jetskiëen',
+            'activeit-eten' => true,
+            'activiteitmindeelnemers' => 10,
+            'activiteitmaxdeelnemers' => 20,
+            'activiteitkosten' => 30.00,
+            'activiteitbenodigheden' => 'rijbewijs',
+            'activiteitbegintijd' => '14:00',
+            'activiteiteindtijd' => '16:00',
+            'activiteitafbeelding' => 'https://www.trackjackeurope.com/wp-content/uploads/2020/09/jetski.jpg',
+        ]);
     }
 }
