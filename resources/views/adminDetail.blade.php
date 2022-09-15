20f5d270fdd86aa68affc06ed264be13ae0d6a2b
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-09-15 10:59:11 +0200

    merge fix

--- resources/views/adminDetail.blade.php
+++ resources/views/adminDetail.blade.php
@@ -11,55 +11,54 @@
 
         <div class="form-group">
             <label for="price">afbeelding link</label>
-            <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
-                   value="https://www.trackjackeurope.com/wp-content/uploads/2020/09/jetski.jpg">
+            <input type="text" class="form-control" name="afbeelding"
+                   value="">
         </div>
         <div class="form-group">
             <label for="name">Locatie</label>
-            <input type="text" class="form-control" name="attractionName"
-                   value="stroombroek in braamt">
+            <input type="text" class="form-control" name="locatie"
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">inclusief eten </label>
-            <input type="text" class="form-control" name="attractionDescription"
-                   value="ja het evenement heeft eten en drinken erbij">
+            <input type="text" class="form-control" name="eten"
+                   value="">
         </div>
 
         <div class="form-group">
             <label for="price">minimaal aantal deelnemers</label>
-            <input type="text" class="form-control" name="attractionImage"
-                   value="er moeten minimaal 10 mense zich aanmelden
-                    voor dit evenement en er kunnen maximaal 30 mensen hieraan deelnemen">
+            <input type="text" class="form-control" name="mindeelnemers"
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">max deelnemers</label>
             <input type="text" class="form-control" name="attractionLoation"
-                   value="0">
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">kosten</label>
             <input type="text" class="form-control" name="attractionWaittime"
-                   value="voor deze activiteit komen er kosten voor de gebruiker van 30 euro p.p.">
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">benodigenheden</label>
             <input type="text" class="form-control" name="attractionMinLength"
-                   value="ja hebt hier een rijbewijs voor nodig anders mag je niet op een jet ski">
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">begintijd</label>
             <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
-                   value="14:00">
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">eindtijd</label>
             <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
-                   value="16:00">
+                   value="">
         </div>
         <div class="form-group">
             <label for="price">omschrijving</label>
             <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
-                   value="Hier nog eventuele informatie van de activiteit">
+                   value="">
         </div>
 
         <button type="submit" class="btn btn-primary mb-2">voeg activiteit</button>
