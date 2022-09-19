77eb16857a6761a5190850ea2ee5f77f44b314ea
D D <derksendhk@gmail.com>
2022-09-19 13:17:24 +0200

    Inlog controller

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,15 @@
 @extends('layout')
-
 @section('content')
+
+
+
+<div id="map-container-google-2" class="z-depth-1-half map-container" style="height: 700px">
+    <iframe src="https://maps.google.com/maps?q=doetinchem&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+            style="border:0" allowfullscreen></iframe>
+</div>
+
+
+
+
+
 @endsection
