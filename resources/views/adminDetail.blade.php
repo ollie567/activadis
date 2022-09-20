2013ce438cc17d2dd3d61e68a82e531b34f66424
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-09-20 11:53:16 +0200

    list style

--- resources/views/adminDetail.blade.php
+++ resources/views/adminDetail.blade.php
@@ -27,23 +27,23 @@
         </div>
         <div class="form-group">
             <label for="price">inclusief eten </label>
-            <input type="radio" class="form-control" name="eten" value="ja">
-            <input type="radio" class="form-control" name="eten" value="nee">
+            <p style="display: flex"><input type="radio" class="form-control" name="eten" value="ja" style="width: 75px">ja</p>
+            <p style="display: flex"><input type="radio" class="form-control" name="eten" value="ja" style="width: 75px">nee</p>
         </div>
 
         <div class="form-group">
             <label for="price">minimaal aantal deelnemers</label>
-            <input type="number" class="form-control" name="mindeelnemers"
+            <input type="number" class="form-control" name="mindeelnemers" required
                    value="0">
         </div>
         <div class="form-group">
             <label for="price">max deelnemers</label>
-            <input type="number" class="form-control" name="maxdeelnemers"
+            <input type="number" class="form-control" name="maxdeelnemers" required
                    value="10">
         </div>
         <div class="form-group">
             <label for="price">kosten</label>
-            <input type="text" class="form-control" name="kosten"
+            <input type="text" class="form-control" name="kosten" required
                    value="">
         </div>
         <div class="form-group">
@@ -53,17 +53,17 @@
         </div>
         <div class="form-group">
             <label for="price">begintijd</label>
-            <input type="text" class="form-control" name="btijd"
+            <input type="text" class="form-control" name="btijd" required
                    value="">
         </div>
         <div class="form-group">
             <label for="price">eindtijd</label>
-            <input type="text" class="form-control" name="etijd"
+            <input type="text" class="form-control" name="etijd" required
                    value="">
         </div>
         <div class="form-group">
             <label for="price">omschrijving</label>
-            <input type="text" class="form-control" name="omschrijving"
+            <input type="text" class="form-control" name="omschrijving" required
                    value="">
         </div>
 
@@ -84,39 +84,39 @@
                 <input type="hidden" value="{{$activ->ID}}" name="id">
                 <div class="form-group">
                     <label for="price">afbeelding link</label>
-                    <input type="text" class="form-control" name="afbeelding"
+                    <input type="text" class="form-control" name="afbeelding" required
                            value="{{$activ->activiteitafbeelding}}">
                 </div>
                 <div class="form-group">
                     <label for="price">Naam</label>
-                    <input type="text" class="form-control" name="naam"
+                    <input type="text" class="form-control" name="naam" required
                            value="{{$activ->activiteitnaam}}">
                 </div>
                 <div class="form-group">
                     <label for="name">Locatie</label>
-                    <input type="text" class="form-control" name="locatie"
+                    <input type="text" class="form-control" name="locatie" required
                            value="{{$activ->activiteitlocatie}}">
                 </div>
                 <div class="form-group">
                     <label for="price">inclusief eten </label>
-                    <input type="text" class="form-control" name="attractionDescription"
+                    <input type="text" class="form-control" name="attractionDescription" required
                            value="{{$activ->activeiteten}}">
 {{--                              value="<?php echo $eten?> ">--}}
                 </div>
 
                 <div class="form-group">
                     <label for="price">minimaal aantal deelnemers</label>
-                    <input type="text" class="form-control" name="attractionImage"
+                    <input type="text" class="form-control" name="attractionImage" required
                            value="{{$activ->activiteitmindeelnemers}}">
                 </div>
                 <div class="form-group">
                     <label for="price">max deelnemers</label>
-                    <input type="text" class="form-control" name="attractionLoation"
+                    <input type="text" class="form-control" name="attractionLoation" required
                            value="{{$activ->activiteitmaxdeelnemers}}">
                 </div>
                 <div class="form-group">
                     <label for="price">kosten</label>
-                    <input type="text" class="form-control" name="attractionWaittime"
+                    <input type="text" class="form-control" name="attractionWaittime" required
                            value="{{$activ->activiteitkosten}}">
                 </div>
                 <div class="form-group">
@@ -126,17 +126,17 @@
                 </div>
                 <div class="form-group">
                     <label for="price">begintijd</label>
-                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
+                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor" required
                            value="{{$activ->activiteitbegintijd}}">
                 </div>
                 <div class="form-group">
                     <label for="price">eindtijd</label>
-                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
+                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor" required
                            value="{{$activ->activiteiteindtijd}}">
                 </div>
                 <div class="form-group">
                     <label for="price">omschrijving</label>
-                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor"
+                    <input type="text" class="form-control" name="attractionMinLengthWithSupervisor" required
                            value="{{$activ->activeitomschrijving}}">
                 </div>
 
