cafb7183fa676751dd48515d329b76581b7fab28
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-10-19 09:18:06 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,5 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<?php
-$detail = DB::table('users')->where('ID', $_GET['id'])->get();
-?>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
