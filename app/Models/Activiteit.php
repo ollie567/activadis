68522b1f0706394b1786892b3849ea65789cf292
ollie538 <60739526+ollie567@users.noreply.github.com>
2022-10-19 12:32:19 +0200

    protected fill

--- app/Models/Activiteit.php
+++ app/Models/Activiteit.php
@@ -9,4 +9,20 @@ class Activiteit extends Model
 {
     use HasFactory;
     protected $table = 'activiteiten';
+
+    protected $fillable = [
+        'activiteitnaam',
+        'activiteitlocatie',
+        'activeitomschrijving',
+        'activeiteten',
+        'activiteitmindeelnemers',
+        'activiteitmaxdeelnemers',
+        'activiteitkosten',
+        'activiteitbenodigheden',
+        'activiteitbegintijd',
+        'activiteiteindtijd',
+        'activiteitafbeelding'
+    ];
+
+
 }
